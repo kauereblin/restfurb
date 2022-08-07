0cf2e123c6497a4fd3fb1f0998edd481bc534872
kauereblin <kaue.reblin159@gmail.com>
2022-08-07 17:46:37 -0300

    remove unnecessary files

--- backend/src/OrderRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import OrderController  from './controllers/OrderController';
-
-const routes = Router();
-
-
-export default routes;
--- backend/src/UserRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import UserController  from './controllers/UserController';
-
-const routes = Router();
-
-
-export default routes;
